dd5965aafb3b198048e4056ded1347e9e377e1c6
lanqwertydev <lanqwertydev@gmail.com>
2025-07-30 14:25:05 -0400

    ejercicio 1

---
+++ retos_sesion_04/ejercicio_01.py
@@ -0,0 +1,7 @@
+# Crea un archivo "ejercicio_01.py", añadir un comentario con la descripcion de del ejercicio "1" y crea un cuadro con prints
+
+print ("********")
+print ("*      *")
+print ("*      *")
+print ("*      *")
+print ("********")
