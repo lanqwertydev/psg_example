f3052d832b7589d3dcdd6a60a5efe82da62fe9e5
lanqwertydev <lanqwertydev@gmail.com>
2025-07-31 00:01:42 -0400

    ejercicios

---
+++ retos_sesion_05/ejercicio_02.py
@@ -0,0 +1,22 @@
+
+# C = (F -32) * 1/1.8
+
+# F = (C * 1.8) + 32
+
+# F = C * 9/5 + 32
+
+c = 30 
+
+F = (c * (9/5) + 32 )
+
+print ("30 C es igual a " + str(F) + " Fahrenheit") 
+
+c = -273.99
+F = (c * (9/5) + 32 )
+
+print ("-273.99 C es igual a " + str (F) + "Fahrenheit")
+
+c = 100
+F = (c * (9/5) + 32 )
+
+print ("100 C es igual a " + str(F) + " Fahrenheit")
---
+++ retos_sesion_05/ejercicio_03.py
@@ -0,0 +1,13 @@
+segundos = 288325
+
+minutos = int (int (segundos) / 60)
+
+print ("son " + str(minutos) + " minutos")
+
+horas = int( minutos / 60 )
+
+print ("son " + str(horas) + " horas")
+
+dias = int (horas / 24)
+
+print ("son " + str(dias) + " dias")
