76d350e5cb429de2a9dcec65574fa06f986da5c2
lanqwertydev <lanqwertydev@gmail.com>
2025-08-01 13:35:07 -0400

    luces

---
+++ retos_sesion_06/ejercicio_02.py
@@ -0,0 +1,4 @@
+a = True
+b = False
+
+print (not ((a or b) and not (a and b)))
---
+++ retos_sesion_06/ejercicio_03.py
@@ -0,0 +1,12 @@
+
+a = True  #Interruptor 1
+b = True  #Interruptor 2
+
+
+print (a and b )
+
+a = True 
+
+b = False
+
+print (a and b)
